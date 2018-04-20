d0823533c7a0e3f3e038e07afe6e1fec50381ac7
Skyelah Bay <skyelahbay@gmail.com>
2018-04-19 20:47:14 -0500

    update toDo.js with v1 reqs

--- js/toDo.js
+++ js/toDo.js
@@ -1,2 +1,29 @@
-// create a variable to hold the list items for our to do list.
+// create a variable to hold the list items for our to do list. could use an object, but lab calls for an array
 let toDoList = [];
+
+// then we need a way to display the items in that list. let's call that function displayItem();
+
+// after that, we need a way to add a new item to the to do list. We can do that via a function as well, addItem();
+function displayItems(list){ //take our toDoList array as an argument.
+  return console.log(list);
+}
+function addItem(item){ //let it take an argument that is the item as a string.
+  return toDoList.push(item);
+}
+function changeItem(n, input){ //take two arguments: the index to modify, and the new input.
+  return toDoList[n] = input;
+}
+function removeItem(n){ //one argument: starting index of splice.
+  toDoList.splice(n, 1);
+  return toDoList;
+}
+
+
+//now if we want to edit a pre-existing item on the list, we can do this simply changing the array index itself,
+//which we can put in another function for usability later. changeItem();
+//changeItem(2, 'flowers'); would change the 3rd item in the array.
+
+//finally, in v1.0 we want to add a way to delete an item. We can do this via splice in a function as well. removeItem();
+//removeItem(1,1); would remove only the second item.
+
+
