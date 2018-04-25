40cf16fee9ce2592a4ae7436d9bdf047d25ff58f
Skyelah Bay <skyelahbay@gmail.com>
2018-04-24 20:47:13 -0500

    Update toDo.js
    give a status about toDo item completion, update displayItems() method.

--- js/toDo.js
+++ js/toDo.js
@@ -1,9 +1,21 @@
-// create an object to hold the list items for our to do list. as well as the methods to alter the list.
+/*  V5 Reqs: 
+  displayItems should show toDoList.textValue
+  displayItems should tell you if your toDoList is empty
+  displayItems should show you .completed.
+*/
+
 let toDoList = {
   list: [],
-  displayItems: function(list){
-    return this.list; 
-  },
+  displayItems: function(){
+    if(this.list.length < 1){return 'Your todo list is empty!';}
+    else{
+        for(let i = 0; i < this.list.length; i++){
+          let status; //we only use status here so we can just assign it for this block.
+          this.list[i].completed === true ? status = 'completed' : status = 'incomplete'; //I used ternary because condensed.
+          console.log(`${this.list[i].textValue}: ${status}`); 
+        }//end for loop
+    }//end else
+  }, //end function
   addItem: function(itemText){ //instead of pushing text, push an object instead
    this.list.push({
     textValue: itemText,
@@ -22,7 +34,22 @@ let toDoList = {
   toggleCompleted: function(n){
     let booli = this.list[n]; //set to a var for easier access below.
     booli.completed = !booli.completed;
-    return this.list;
+  },
+  toggleAll: function(){
+    for(let i = 0, count = 0 i < this.list.length; i++){ //for each item in the list
+      if(this.list[i].completed === true){ // if current item is completed, increase count
+       count++; 
+      }
+    }
+    if(count === this.list.length){ // if all items are completed
+      for(let i = 0; i < this.list.length; i++){
+        this.toggleCompleted[i]; // make them all false
+      }
+    }else{
+     for(let i = 0; i < this.list.length; i++){ // otherwise if ANY are not (since count !== this.list.length)
+        this.list[i].completed = true; // make them all true
+     }
+    }
   }
 };
 
